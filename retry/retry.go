729946ef01c6be15cab46abc38c041a283688341
Egbewatt Kokou Mwangah Smith <ksupro1@gmail.com>
2024-03-03 18:12:53 -0700

    Add BackOff - Exp & Jitter match errors with Type and message Tests

--- retry/retry.go
+++ retry/retry.go
@@ -2,23 +2,74 @@ package retry
 
 import (
 	"errors"
+	"math/rand"
 	"reflect"
 	"sync/atomic"
 	"time"
+
 )
+
+// BackOff
+type BackOff struct {
+	delay       time.Duration
+	exponential bool
+	jitter      bool
+	randomness  *rand.Rand
+}
+
+// BackOff
+func NewBackOff(Delay time.Duration, exponential bool, jitter bool) *BackOff {
+	return &BackOff{
+		delay:       Delay,
+		exponential: exponential,
+		jitter:      jitter,
+		randomness:  rand.New(rand.NewSource(time.Now().UnixNano())),
+	}
+}
+
+// Triggers time.sleep() based of current number of retries & BackOff configuration - regular | jitter | exponential | exponential jitter
+func (b *BackOff) Delay(attemptIteration int32) {
+	if !b.jitter && !b.exponential {
+		time.Sleep(time.Duration(attemptIteration) * b.delay)
+	}
+	if b.jitter && b.exponential {
+		sleepValue := uint64(b.delay.Seconds())
+		sleepTime := (time.Duration(attemptIteration) * time.Duration(sleepValue)) + time.Duration(float64(b.randomness.Int63())/(1<<63))
+		time.Sleep(sleepTime)
+	}
+	if b.jitter {
+		//
+		// scale down the 63-bit integer value to a floating-point number between 0 and 1.0.
+		// multiply by sleep duration in seconds to get the actual delay in milliseconds.
+		sleepTime := (time.Duration(attemptIteration) * b.delay) + time.Duration(float64(b.randomness.Int63())/(1<<63)) // 1 << 63 - bitwise left shift operation (2^63)
+		time.Sleep(sleepTime)
+	}
+	if b.exponential {
+		sleepValue := uint64(b.delay.Seconds())
+		time.Sleep(time.Duration(attemptIteration) * time.Duration(atomic.AddUint64(&sleepValue, 1)))
+	}
+}
 
 // Retry
 type Retry struct {
-	delay       time.Duration
 	attempts    *atomic.Int32
 	maxAttempts int32
 	onErrors    []error
+	backoff     *BackOff
 }
 
 // New Retry  instance with given delay, max attempts and a list of errors to watch for to trigger a retry. If no error is matched panic
-func New(maxAttempts int32, delay time.Duration, watchErrors []error) *Retry {
+func New(maxAttempts int32, delay time.Duration, watchErrors []error, backOff *BackOff) *Retry {
 	return &Retry{
-		delay:       delay,
+		attempts:    &atomic.Int32{},
+		maxAttempts: maxAttempts,
+		onErrors:    watchErrors,
+		backoff:     backOff,
+	}
+}
+
+func NewWithBackOff(maxAttempts int32, delay time.Duration, watchErrors []error) *Retry {
+	return &Retry{
 		attempts:    &atomic.Int32{},
 		maxAttempts: maxAttempts,
 		onErrors:    watchErrors,
@@ -35,7 +86,7 @@ func (r *Retry) Do(fn func() error) error {
 			return nil
 		}
 
-		if !r.isWatchedError(err) {
+		if !isErrorWatchedByRetry(err, r) {
 			panic(err)
 		}
 
@@ -45,15 +96,18 @@ func (r *Retry) Do(fn func() error) error {
 
 		// increment attempt and delay till next iteration
 		r.attempts.Add(1)
-		time.Sleep(r.delay)
+		r.backoff.Delay(r.attempts.Load())
 	}
 }
 
-// check if the given error is in the list of watched errors. Used to catch mostly transient errors for retry scenario
-func (r *Retry) isWatchedError(err error) bool {
-	for _, error := range r.onErrors {
-		if reflect.TypeOf(error) == reflect.TypeOf(&err) {
-			//one of the errors we watching for
+// check if the given error is in the list of watched errors.
+// Used to catch mostly transient errors for retry scenario and
+// returns true when the error matches one of the provided errors by Type and Messages
+func isErrorWatchedByRetry(err error, r *Retry) bool {
+	for _, watchedError := range r.onErrors {
+		// match by type and messages
+		if reflect.TypeOf(err) == reflect.TypeOf(watchedError) && err.Error() == watchedError.Error() {
+			// One of the errors we are watching for
 			return true
 		}
 	}
